7e25fa4882ab897326ee15d107c519887a334302
whathebea <bea.graciast@gmail.com>
2022-12-22 21:33:39 -0300

    feat: finished task controller

--- src/main/java/com/whathebea/taskmanagement/controller/TaskController.java
+++ src/main/java/com/whathebea/taskmanagement/controller/TaskController.java
@@ -1,4 +1,55 @@
 package com.whathebea.taskmanagement.controller;
 
+import com.whathebea.taskmanagement.model.Task;
+import com.whathebea.taskmanagement.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable long id) {
+        Optional<Task> task = taskService.getTask(id);
+        return task.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable long id, @RequestBody Task task) {
+        Optional<Task> taskOptional = taskService.getTask(id);
+        if (taskOptional.isPresent()) {
+            Task updatedTask = taskService.updateTask(task);
+            return ResponseEntity.ok().body(updatedTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable long id) {
+        Optional<Task> taskOptional = taskService.getTask(id);
+        if (taskOptional.isPresent()) {
+            taskService.deleteTask(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
