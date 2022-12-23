0f8c5f52d97adbc7c59c1d221b2ca29db7f8e2e5
whathebea <bea.graciast@gmail.com>
2022-12-22 21:13:20 -0300

    feat: created the service class

--- src/main/java/com/whathebea/taskmanagement/service/TaskService.java
+++ src/main/java/com/whathebea/taskmanagement/service/TaskService.java
@@ -1,4 +1,34 @@
 package com.whathebea.taskmanagement.service;
 
+import com.whathebea.taskmanagement.model.Task;
+import com.whathebea.taskmanagement.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> getTask(long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
