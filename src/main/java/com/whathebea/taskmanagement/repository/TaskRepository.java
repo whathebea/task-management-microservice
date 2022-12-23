9637f71b8081291c313371aa8443f6cfdfa5adbc
whathebea <bea.graciast@gmail.com>
2022-12-22 21:11:02 -0300

    feat: created the repository class

--- src/main/java/com/whathebea/taskmanagement/repository/TaskRepository.java
+++ src/main/java/com/whathebea/taskmanagement/repository/TaskRepository.java
@@ -1,4 +1,17 @@
 package com.whathebea.taskmanagement.repository;
 
-public class TaskRepository {
+import com.whathebea.taskmanagement.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT t FROM Task t WHERE t.status = :status")
+    List<Task> findByStatus(@Param("status") Task.TaskStatus status);
+
 }
